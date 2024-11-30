7369a11f56c6069ca952061ff3f0dce8a1683828
jreyesr <jreyesr@users.noreply.github.com>
2024-11-30 15:55:16 -0500

    Add setting to control page segmentation mode (block, column, single line, single word, or sparse text)

--- nodes/TesseractNode/TesseractNode.node.ts
+++ nodes/TesseractNode/TesseractNode.node.ts
@@ -6,7 +6,7 @@ import {
 	NodeOperationError
 } from 'n8n-workflow';
 import {extractBoxes, performOCR} from "./operations";
-import {createWorker} from "tesseract.js";
+import {createWorker, PSM} from "tesseract.js";
 
 export class TesseractNode implements INodeType {
 	description: INodeTypeDescription = {
@@ -152,6 +152,40 @@ export class TesseractNode implements INodeType {
 						default: 'eng',
 						description: 'Choose from the lang codes in https://tesseract-ocr.github.io/tessdoc/Data-Files#data-files-for-version-400-november-29-2016',
 						noDataExpression: true
+					},
+					{
+						displayName: 'Page Segmentation Mode',
+						name: 'psm',
+						type: 'options',
+						default: 'SINGLE_BLOCK',
+						description: 'For a description of the modes, see <a href="https://pyimagesearch.com/2021/11/15/tesseract-page-segmentation-modes-psms-explained-how-to-improve-your-ocr-accuracy/">this link</a>',
+						options: [
+							{
+								name: 'Single Block',
+								value: 'SINGLE_BLOCK',
+								description: 'Assume a single uniform block of text, such as a book page',
+							},
+							{
+								name: 'Single Column',
+								value: 'SINGLE_COLUMN',
+								description: 'Assume a single column of text of variable sizes, such as a table, chapter index or invoice'
+							},
+							{
+								name: 'Single Line',
+								value: 'SINGLE_LINE',
+								description: 'Treat the image as a single text line'
+							},
+							{
+								name: 'Single Word',
+								value: 'SINGLE_WORD',
+								description: 'Treat the image as a single word'
+							},
+							{
+								name: 'Sparse Text',
+								value: 'SPARSE_TEXT',
+								description: 'Find as much text as possible in no particular order. Use when text is scattered across the image.'
+							},
+						]
 					}
 				]
 			}
@@ -162,7 +196,10 @@ export class TesseractNode implements INodeType {
 		const items = this.getInputData();
 		const operation = this.getNodeParameter('operation', 0, 'ocr') as 'ocr' | 'boxes';
 		const lang = this.getNodeParameter('options.language', 0, 'eng') as string;
+		const psm = this.getNodeParameter('options.psm', 0, 'SINGLE_BLOCK') as 'SINGLE_BLOCK' | 'SINGLE_COLUMN' | 'SINGLE_LINE' | 'SINGLE_WORD' | 'SPARSE_TEXT';
+
 		const worker = await createWorker(lang);
+		await worker.setParameters({tessedit_pageseg_mode: PSM[psm]})
 
 		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
 			try {
